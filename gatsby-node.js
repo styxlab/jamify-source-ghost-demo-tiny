31e65df714558584a40361295df11c0229e72c6c
cws <cws@home.fritz.box>
2020-06-15 12:23:33 +0200

    fix error

--- gatsby-node.js
+++ gatsby-node.js
@@ -27,7 +27,7 @@ exports.createPages = async ({ graphql, actions }) => {
   // Create blog posts pages.
   const posts = result.data.allGhostPost.edges
 
-  posts.forEach((post, index) => {
+  posts.forEach(({ node }, index) => {
     const previous = index === posts.length - 1 ? null : posts[index + 1].node
     const next = index === 0 ? null : posts[index - 1].node
 
